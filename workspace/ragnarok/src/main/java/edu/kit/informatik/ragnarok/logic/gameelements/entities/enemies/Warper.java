20d1e6ff1af0c4bf1eecebddda3741c7d52c53d2
Angelo Aracri <angeloaracri@yahoo.de>
2016-06-16 20:12:47 +0200

    Added some fancy Particles to warper!

--- workspace/ragnarok/src/main/java/edu/kit/informatik/ragnarok/logic/gameelements/entities/enemies/Warper.java
+++ workspace/ragnarok/src/main/java/edu/kit/informatik/ragnarok/logic/gameelements/entities/enemies/Warper.java
@@ -5,6 +5,8 @@ import org.eclipse.swt.graphics.RGB;
 import edu.kit.informatik.ragnarok.gui.Field;
 import edu.kit.informatik.ragnarok.logic.gameelements.GameElement;
 import edu.kit.informatik.ragnarok.logic.gameelements.entities.Entity;
+import edu.kit.informatik.ragnarok.logic.gameelements.entities.particles.ParticleSpawner;
+import edu.kit.informatik.ragnarok.logic.gameelements.entities.particles.ParticleSpawnerOption;
 import edu.kit.informatik.ragnarok.primitives.Direction;
 import edu.kit.informatik.ragnarok.primitives.Frame;
 import edu.kit.informatik.ragnarok.primitives.TimeDependency;
@@ -13,9 +15,30 @@ import edu.kit.informatik.ragnarok.primitives.Vec2D;
 public class Warper extends Entity {
 
 	private TimeDependency warpAction = new TimeDependency(1);
+	private ParticleSpawner warpParticles = null;
 	
 	public Warper(Vec2D startPos) {
 		super(startPos);
+		
+		// if this is first instantiated warper: create explosion Particle
+		if (warpParticles == null) {
+			warpParticles = new ParticleSpawner();
+			
+			warpParticles.angle = new ParticleSpawnerOption(0, (float) (2 * Math.PI), (float) (2 * Math.PI), (float) (4 * Math.PI));
+			
+			warpParticles.colorR = new ParticleSpawnerOption(250, 0);
+			warpParticles.colorG = new ParticleSpawnerOption(250, -250);
+			warpParticles.colorB = new ParticleSpawnerOption(150);
+			warpParticles.colorA = new ParticleSpawnerOption(220, -220);
+			
+			warpParticles.timeMin = 1f;
+			warpParticles.timeMin = 1f;
+			
+			warpParticles.amountMin = 5;
+			warpParticles.amountMax = 8;
+			
+			warpParticles.speed = new ParticleSpawnerOption(2, 3, -1, 1);
+		}
 	}
 
 	@Override
@@ -23,12 +46,11 @@ public class Warper extends Entity {
 		float progress = warpAction.getProgress();
 		
 		for (float i = 1; i >= 0.2; i-=0.1) {
-			RGB innerColor = new RGB((int)(250 * (progress) * i), (int)(250 * (1-progress)), (int)(150));
+			RGB innerColor = new RGB((int)(250 * i), (int)(250 * (1-progress)), (int)(150));
 			
 			// draw body
 			f.drawCircle(this.getPos(), this.getSize().multiply(i), innerColor);
 		}
-		
 		
 	}
 	
@@ -50,6 +72,9 @@ public class Warper extends Entity {
 			
 			// get target (player)
 			Vec2D target = this.getGameModel().getPlayer().getPos();
+			
+			// animate particles
+			this.warpParticles.spawn(this.getGameModel(), this.getPos());
 			
 			// determine if x or y is greater in distance  
 			Vec2D dif = this.getPos().add(target.multiply(-1)); 
