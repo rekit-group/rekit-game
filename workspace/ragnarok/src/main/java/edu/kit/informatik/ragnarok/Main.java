1ec5310185cfd381489e8b89bf513a2049800449
Dominik Fuchß <fuchss.dominik@gmail.com>
2016-06-17 17:56:23 +0200

    Moved functionality of Game to Main (for styling issues)

--- workspace/ragnarok/src/main/java/edu/kit/informatik/ragnarok/Main.java
+++ workspace/ragnarok/src/main/java/edu/kit/informatik/ragnarok/Main.java
@@ -1,11 +1,40 @@
 package edu.kit.informatik.ragnarok;
 
+import edu.kit.informatik.ragnarok.controller.Controller;
+import edu.kit.informatik.ragnarok.controller.InputHelper;
+import edu.kit.informatik.ragnarok.gui.GameView;
+import edu.kit.informatik.ragnarok.logic.GameModel;
+
+/**
+ * Game class that instantiates all necessary classes that are required for a
+ * game. implements a singleton to prevent multiple instantiation.
+ *
+ * @author Angelo Aracri
+ * @version 1.0
+ */
 public class Main {
+
 	/**
 	 * Launches the application by starting the game
-	 * @param args not used
+	 *
+	 * @param args
+	 *            not used
 	 */
 	public static void main(String[] args) {
-		Game.getGame();
+		// Create MVC
+		// Set References:
+		// V----> M <----C
+		GameModel model = new GameModel();
+		GameView view = new GameView(model);
+		Controller controller = new Controller(model);
+
+		// Instantiate InputHelper that requires a shell
+		InputHelper.init(view.getCanvas());
+
+		// Start MVC
+		model.start();
+		controller.start();
+		view.start();
+
 	}
 }
