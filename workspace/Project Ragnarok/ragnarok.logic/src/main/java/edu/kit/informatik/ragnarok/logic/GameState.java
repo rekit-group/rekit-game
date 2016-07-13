74d2a4104b2dd5ad1cf77908098d2964476074ab
Dominik Fuchß <fuchss.dominik@gmail.com>
2016-07-13 09:55:46 +0200

    Added JDoc to ReflectUtils and GameState

--- workspace/Project Ragnarok/ragnarok.logic/src/main/java/edu/kit/informatik/ragnarok/logic/GameState.java
+++ workspace/Project Ragnarok/ragnarok.logic/src/main/java/edu/kit/informatik/ragnarok/logic/GameState.java
@@ -1,5 +1,18 @@
 package edu.kit.informatik.ragnarok.logic;
 
+/**
+ * This enum defines the different states of the game
+ * 
+ * @author Dominik Fuchß
+ *
+ */
 public enum GameState {
-	MENU, INGAME
+	/**
+	 * This state indicates that currently a menu is shown by the game
+	 */
+	MENU,
+	/**
+	 * This state indicates that currently a level is shown by the game
+	 */
+	INGAME
 }
--- workspace/Project Ragnarok/ragnarok.logic/src/main/java/edu/kit/informatik/ragnarok/logic/util/ReflectUtils.java
+++ workspace/Project Ragnarok/ragnarok.logic/src/main/java/edu/kit/informatik/ragnarok/logic/util/ReflectUtils.java
@@ -8,7 +8,30 @@ import java.util.Set;
 
 import org.reflections.Reflections;
 
-public class ReflectUtils {
+/**
+ * This class contains several methods for using Java Reflections in a proper
+ * way
+ * 
+ * @author Dominik Fuchß
+ *
+ */
+public final class ReflectUtils {
+	/**
+	 * Prevent instantiation
+	 */
+	private ReflectUtils() {
+	}
+
+	/**
+	 * Load all implementations of a class by search path (-> classpath) <br>
+	 * If a class wants to be loaded, the class needs a default constructor
+	 * 
+	 * @param searchPath
+	 *            the search path (e.g. java.lang)
+	 * @param type
+	 *            the class
+	 * @return a set of instances of the found classes
+	 */
 	@SuppressWarnings("unchecked")
 	public static final <T> Set<T> get(String searchPath, Class<T> type) {
 		Set<Class<? extends T>> classes = new Reflections(searchPath).getSubTypesOf(type);
