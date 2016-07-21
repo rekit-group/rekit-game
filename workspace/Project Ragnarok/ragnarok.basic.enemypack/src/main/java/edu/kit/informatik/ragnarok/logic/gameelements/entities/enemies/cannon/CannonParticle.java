863626c6a53ec1a371c46f6214a7413a2c61f1bc
Angelo Aracri <angeloaracri@yahoo.de>
2016-07-21 19:02:19 +0200

    JavaDoc'd CannonParticle

--- workspace/Project Ragnarok/ragnarok.basic.enemypack/src/main/java/edu/kit/informatik/ragnarok/logic/gameelements/entities/enemies/cannon/CannonParticle.java
+++ workspace/Project Ragnarok/ragnarok.basic.enemypack/src/main/java/edu/kit/informatik/ragnarok/logic/gameelements/entities/enemies/cannon/CannonParticle.java
@@ -1,26 +1,46 @@
 package edu.kit.informatik.ragnarok.logic.gameelements.entities.enemies.cannon;
 
-
 import edu.kit.informatik.ragnarok.logic.gameelements.GameElement;
 import edu.kit.informatik.ragnarok.logic.gameelements.Team;
+import edu.kit.informatik.ragnarok.logic.gameelements.entities.Player;
 import edu.kit.informatik.ragnarok.logic.gameelements.entities.particles.DamageParticle;
 import edu.kit.informatik.ragnarok.logic.gameelements.entities.particles.Particle;
+import edu.kit.informatik.ragnarok.logic.gameelements.inanimate.Inanimate;
 import edu.kit.informatik.ragnarok.primitives.geometry.Direction;
 
+/**
+ * {@link Particle} that extends {@link DamageParticle} that is extended by
+ * calling a {@link Cannon Cannons} method {@link Cannon#hitSomething()} upon
+ * colliding with an {@link Inanimate} or {@link Player}.
+ * 
+ * @author Angelo Aracri
+ */
 public class CannonParticle extends DamageParticle {
-	
+
+	/**
+	 * The parenting {@link Cannon} whose {@link Cannon#hitSomething()} will be
+	 * called upon a collision with an {@link Inanimate} or {@link Player}.
+	 */
 	private Cannon parent;
-	
+
+	/**
+	 * Constructor that saves the reference of the given parenting
+	 * {@link Cannon} whose {@link Cannon#hitSomething()} will be called upon a
+	 * collision with an {@link Inanimate} or {@link Player}.
+	 * 
+	 * @param parent
+	 *            the parenting {@link Cannon} to message.
+	 */
 	public CannonParticle(Cannon parent) {
 		super();
 		this.parent = parent;
 	}
-	
+
 	@Override
 	public Particle clone() {
 		return new CannonParticle(this.parent);
 	}
-	
+
 	@Override
 	public void reactToCollision(GameElement element, Direction dir) {
 		super.reactToCollision(element, dir);
