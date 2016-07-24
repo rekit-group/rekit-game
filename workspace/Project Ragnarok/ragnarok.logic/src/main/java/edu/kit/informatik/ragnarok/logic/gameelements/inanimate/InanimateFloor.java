add73e3914a062af33d4c6ca8feab5596fb370d5
Angelo Aracri <angeloaracri@yahoo.de>
2016-07-24 12:43:40 +0200

    Changed InanimateFloors visualization. What do think? Does it cause performance issues?

--- workspace/Project Ragnarok/ragnarok.logic/src/main/java/edu/kit/informatik/ragnarok/logic/gameelements/inanimate/InanimateFloor.java
+++ workspace/Project Ragnarok/ragnarok.logic/src/main/java/edu/kit/informatik/ragnarok/logic/gameelements/inanimate/InanimateFloor.java
@@ -7,12 +7,21 @@ import edu.kit.informatik.ragnarok.logic.gameelements.entities.particles.Particl
 import edu.kit.informatik.ragnarok.logic.gameelements.entities.particles.ParticleSpawnerOption;
 import edu.kit.informatik.ragnarok.primitives.geometry.Direction;
 import edu.kit.informatik.ragnarok.primitives.geometry.Vec;
-import edu.kit.informatik.ragnarok.primitives.image.RGBAColor;
 import edu.kit.informatik.ragnarok.primitives.image.RGBColor;
+import edu.kit.informatik.ragnarok.primitives.operable.OpProgress;
 
 public class InanimateFloor extends Inanimate {
 
-	private ParticleSpawner dustParticles;
+	private static int LAYERS = 5;
+
+	private static int STRAW_NUM = 10;
+
+	private static RGBColor[] layerCols;
+	private static Vec[] layerVecs;
+
+	private GrassStraw[] straws;
+
+	private static ParticleSpawner dustParticles;
 	private static final ParticleSpawnerOption dustParticleAngleLeft = new ParticleSpawnerOption((float) ((7 / 4f) * Math.PI),
 			(float) ((5 / 4f) * Math.PI), -(float) ((1 / 4f) * Math.PI), 0);
 	private static final ParticleSpawnerOption dustParticleAngleRight = new ParticleSpawnerOption((float) ((1 / 4f) * Math.PI),
@@ -20,63 +29,110 @@ public class InanimateFloor extends Inanimate {
 	private static final ParticleSpawnerOption dustParticleAngleTop = new ParticleSpawnerOption((float) (-(1 / 2f) * Math.PI),
 			(float) ((1 / 2f) * Math.PI), 0, 0);
 
-	protected InanimateFloor(Vec pos, Vec size, RGBAColor color) {
-		super(pos, size, color);
+	static {
+		// define color range for blocks layers
+		RGBColor startCol = new RGBColor(22, 140, 30);
+		RGBColor endCol = new RGBColor(23, 210, 49);
 
-		this.dustParticles = new ParticleSpawner();
-		this.dustParticles.colorR = new ParticleSpawnerOption(this.color.red);
-		this.dustParticles.colorG = new ParticleSpawnerOption(this.color.green);
-		this.dustParticles.colorB = new ParticleSpawnerOption(this.color.blue);
-		this.dustParticles.colorA = new ParticleSpawnerOption(255);
+		// define position range for blocks layers
+		float size = 1;
+		Vec startVec = new Vec(0, -size / 2 + (size / 2) / LAYERS);
+		Vec endVec = new Vec(0, size / 2 - (size / 2) / LAYERS);
 
-		this.dustParticles.amountMin = 8;
-		this.dustParticles.amountMax = 15;
+		// create Progresses from ranges.
+		OpProgress<RGBColor> progCols = new OpProgress<>(startCol, endCol);
+		OpProgress<Vec> progVecs = new OpProgress<>(startVec, endVec);
 
-		this.dustParticles.speed = new ParticleSpawnerOption(2, 3, -1, 1);
+		// fill arrays for each layer
+		layerCols = new RGBColor[LAYERS];
+		layerVecs = new Vec[LAYERS];
+		for (int i = 0; i < LAYERS; i++) {
+			float prog = i / (float) (LAYERS - 1);
+			layerCols[i] = progCols.getNow(prog);
+			layerVecs[i] = progVecs.getNow(prog);
+		}
+
+		// prepare particles
+		dustParticles = new ParticleSpawner();
+		dustParticles.colorR = new ParticleSpawnerOption(layerCols[0].red);
+		dustParticles.colorG = new ParticleSpawnerOption(layerCols[0].green);
+		dustParticles.colorB = new ParticleSpawnerOption(layerCols[0].blue);
+		dustParticles.colorA = new ParticleSpawnerOption(255);
+
+		dustParticles.amountMin = 8;
+		dustParticles.amountMax = 15;
+
+		dustParticles.speed = new ParticleSpawnerOption(2, 3, -1, 1);
+	}
+
+	protected InanimateFloor(Vec pos, Vec size) {
+		super(pos, size, layerCols[0].toRGBA());
+
+		this.straws = new GrassStraw[STRAW_NUM];
+		for (int i = 0; i < STRAW_NUM; i++) {
+			this.straws[i] = new GrassStraw(pos);
+		}
+	}
+
+	public class GrassStraw {
+		private Vec pos;
+		private RGBColor col;
+		private Vec size;
+
+		public GrassStraw(Vec parentPos) {
+			this.pos = parentPos.add(new Vec(GameConf.PRNG.nextFloat() * 0.8f - 0.4f, -0.3f - GameConf.PRNG.nextFloat() * 0.4f));
+			this.size = new Vec(0.06f, 0.3f + GameConf.PRNG.nextFloat() * 0.4f);
+			this.col = layerCols[GameConf.PRNG.nextInt(layerCols.length)];
+		}
+
+		public void internalRender(Field f) {
+			f.drawRectangle(this.pos, this.size, this.col);
+		}
 	}
 
 	@Override
 	public void internalRender(Field f) {
-		Vec pos = this.getPos();
-		Vec size = this.getSize();
-
-		f.drawRectangle(pos, size, this.color);
-
-		RGBColor darkColor = new RGBColor(this.color.red - 30, this.color.green - 30, this.color.blue - 30);
-
-		float plateThickness = 0.1f;
-		f.drawRectangle(pos.add(new Vec(0, -size.getY() / 2f + plateThickness / 2f)), size.setY(plateThickness), darkColor);
-
+		// Draw rectangles that this Floor is composed of
+		for (int i = 0; i < LAYERS; i++) {
+			f.drawRectangle(this.getPos().add(layerVecs[i]), this.getSize().setY(1 / (float) LAYERS), layerCols[i]);
+		}
+		// Draw GrassStraws
+		for (GrassStraw straw : this.straws) {
+			straw.internalRender(f);
+		}
 	}
 
 	@Override
 	public void reactToCollision(GameElement element, Direction dir) {
 
 		if (Math.abs(element.getVel().getY()) > 6) {
-			this.dustParticles.angle = InanimateFloor.dustParticleAngleTop;
-			this.dustParticles.spawn(this.getScene(), this.getPos().addY(-this.getSize().getY() / 2));
+			dustParticles.angle = InanimateFloor.dustParticleAngleTop;
+			dustParticles.spawn(this.getScene(), this.getPos().addY(-this.getSize().getY() / 2));
 		}
 		// if strong velocity in x direction
 		else if (Math.abs(element.getVel().getX()) > 5) {
 			// if moving right
 			if (element.getVel().getX() > 0) {
-				this.dustParticles.angle = InanimateFloor.dustParticleAngleLeft;
+				dustParticles.angle = InanimateFloor.dustParticleAngleLeft;
 			} else {
-				this.dustParticles.angle = InanimateFloor.dustParticleAngleRight;
+				dustParticles.angle = InanimateFloor.dustParticleAngleRight;
 			}
 
 			Vec pos = this.getPos().addY(-this.getSize().getY() / 2).setX(element.getPos().getX());
 
-			this.dustParticles.spawn(this.getScene(), pos);
+			dustParticles.spawn(this.getScene(), pos);
 		}
 
 		super.reactToCollision(element, dir);
 	}
 
 	public static Inanimate staticCreate(Vec pos) {
-		int randColG = (int) (GameConf.PRNG.nextDouble() * 100 + 100);
-		int randColRB = (int) (GameConf.PRNG.nextDouble() * 40 + 30);
-		return new InanimateFloor(pos, new Vec(1, 1), new RGBAColor(randColRB, randColG, randColRB, 255));
+		return new InanimateFloor(pos, new Vec(1, 1));
+	}
+
+	@Override
+	protected int getOrderZ() {
+		return 10;
 	}
 
 }
