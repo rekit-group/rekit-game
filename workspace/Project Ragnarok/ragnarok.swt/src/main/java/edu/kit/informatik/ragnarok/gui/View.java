28d53c96c59f9866af4a36ce315d3bece42485cf
Dominik Fuchß <fuchss.dominik@gmail.com>
2017-01-21 15:37:41 +0100

    Moved SWT to seperate module

---
+++ workspace/Project Ragnarok/ragnarok.swt/src/main/java/edu/kit/informatik/ragnarok/gui/View.java
@@ -0,0 +1,37 @@
+package edu.kit.informatik.ragnarok.gui;
+
+import edu.kit.informatik.ragnarok.logic.Model;
+import edu.kit.informatik.ragnarok.util.InputHelper;
+
+/**
+ * This Interface defines the View for the MVC.
+ *
+ * @author Dominik Fuchss
+ *
+ */
+public interface View {
+	/**
+	 * Get the view.
+	 *
+	 * @param model
+	 *            the model
+	 * @return the view
+	 */
+	static View getView(Model model) {
+		return new GameView(model);
+	}
+
+	/**
+	 * Start the view.
+	 */
+	void start();
+
+	/**
+	 * Attach a {@link InputHelper}.
+	 *
+	 * @param inputHelper
+	 *            the inputHelper
+	 */
+	void attachMe(InputHelper inputHelper);
+
+}
