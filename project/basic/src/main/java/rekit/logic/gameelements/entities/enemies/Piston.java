bd839f0ba4c91ef433d2674e9ee73504501c77b6
Angelo Aracri <angeloaracri@yahoo.de>
2017-04-10 03:35:31 +0200

    Added config

--- project/basic/src/main/java/rekit/logic/gameelements/entities/enemies/Piston.java
+++ project/basic/src/main/java/rekit/logic/gameelements/entities/enemies/Piston.java
@@ -16,6 +16,7 @@ import rekit.primitives.geometry.Frame;
 import rekit.primitives.geometry.Polygon;
 import rekit.primitives.geometry.Vec;
 import rekit.primitives.image.RGBAColor;
+import rekit.primitives.time.Progress;
 import rekit.primitives.time.Timer;
 import rekit.util.ReflectUtils.LoadMe;
 
@@ -28,6 +29,76 @@ import rekit.util.ReflectUtils.LoadMe;
 @LoadMe
 @SetterInfo(res = "conf/piston")
 public final class Piston extends Enemy implements Configurable {
+	
+	/**
+	 * The height of the non-moving base of the piston
+	 */
+	private static float BASE_HEIGHT;
+	/**
+	 * The short distance between piston tip and the actual defined {@link Piston.expansionLength}
+	 */
+	private static float LOWER_MARGIN;
+
+	/**
+	 * The color of the non-moving base of the piston
+	 */
+	private static RGBAColor BASE_COLOR;
+	
+	/**
+	 * The color of the moving part of the piston
+	 */
+	private static RGBAColor PISTON_COLOR;
+
+	/**
+	 * The minimum and maximum time the piston stays still in open state in milliseconds.
+	 * See how the actual time can be defined in the parameters of the {@link Piston.Piston constructor}.
+	 */
+	private static Progress OPEN_TIME;
+	
+	/**
+	 * The minimum and maximum time the piston stays still in closed state in milliseconds.
+	 * See how the actual time can be defined in the parameters of the {@link Piston.Piston constructor}.
+	 */
+	private static Progress CLOSED_TIME;
+
+	/**
+	 * The minimum and maximum movement speed while opening and closing the piston in units per second.
+	 * See how the actual speed can be defined in the parameters of the {@link Piston.Piston constructor}.
+	 */
+	private static Progress MOVEMENT_SPEED;
+	
+	/**
+	 * The length in units, the piston expands.
+	 * The actual size varies by optical means such as BASE_HEIGHT and LOWER_MARGIN. 
+	 */
+	private int expansionLength;
+	
+	/**
+	 * The direction that piston is directed to.
+	 */
+	private Direction direction;
+	
+	/**
+	 * The actual time the piston stays still while being open in milliseconds 
+	 */
+	private long openTime;
+	
+	/**
+	 * The actual time the piston stays still while being closed in milliseconds 
+	 */
+	private long closedTime;
+	
+	/**
+	 * The movement speed while opening and closing the piston in units per second.
+	 */
+	private long movementSpeed;
+	
+	/**
+	 * The id of the phase to start with.
+	 */
+	private int startPhase;
+	
+	
 	/**
 	 * Prototype Constructor.
 	 */
@@ -69,9 +140,9 @@ public final class Piston extends Enemy implements Configurable {
 		if (options.length >= 1 && options[0] != null && options[0].matches("(\\+|-)?[0-3]+")) {
 			int opt = Integer.parseInt(options[0]);
 			if (opt >= 0 && opt < Direction.values().length) {
-				inst.setDirection(Direction.values()[opt]);
+				// Do sth
 			} else {
-				GameConf.GAME_LOGGER.error("RektKiller was supplied invalid option " + options[0] + " at index 0 for Direction");
+				GameConf.GAME_LOGGER.error("");
 			}
 		}
 
