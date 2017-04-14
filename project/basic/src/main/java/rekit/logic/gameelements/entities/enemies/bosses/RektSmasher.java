6e4e6f1239ad6350caebf4876d77ae0f2877d63c
Dominik Fuchß <develop@fuchss.org>
2017-04-14 23:37:37 +0200

    Fixed format.

--- project/basic/src/main/java/rekit/logic/gameelements/entities/enemies/bosses/RektSmasher.java
+++ project/basic/src/main/java/rekit/logic/gameelements/entities/enemies/bosses/RektSmasher.java
@@ -171,11 +171,16 @@ public final class RektSmasher extends Boss implements Configurable {
 		String i = Inanimate.class.getSimpleName();
 		String n = null;
 		String[][] struct = new String[][] { //
-				{ i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i }, { i, n, n, n, n, n, i, n, n, n, i, i, n, n, n, i, n, n, n, n, n, n },
-				{ i, n, n, n, n, n, i, n, n, n, n, n, n, n, n, i, n, n, n, n, n, n }, { i, i, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, i, n },
-				{ i, i, i, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, i, i, n }, { n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n },
-				{ n, n, n, n, n, n, n, i, n, i, i, i, i, n, i, n, n, n, n, n, n, n }, { n, n, n, n, n, n, n, i, n, i, i, i, i, n, i, n, n, n, n, n, n, n },
-				{ i, i, i, i, i, i, i, i, n, i, i, i, i, n, i, i, i, i, i, i, i, i } };
+				{ i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i }, //
+				{ i, n, n, n, n, n, i, n, n, n, i, i, n, n, n, i, n, n, n, n, n, n }, //
+				{ i, n, n, n, n, n, i, n, n, n, n, n, n, n, n, i, n, n, n, n, n, n }, //
+				{ i, i, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, i, n }, //
+				{ i, i, i, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, i, i, n }, //
+				{ n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n }, //
+				{ n, n, n, n, n, n, n, i, n, i, i, i, i, n, i, n, n, n, n, n, n, n }, //
+				{ n, n, n, n, n, n, n, i, n, i, i, i, i, n, i, n, n, n, n, n, n, n }, //
+				{ i, i, i, i, i, i, i, i, n, i, i, i, i, n, i, i, i, i, i, i, i, i }//
+		};
 
 		BossStructure structure = new BossStructure(struct, this);
 		this.setBossStructure(structure);
