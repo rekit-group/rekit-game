ddf925b33c2816250b4e7f597876b5f47c572558
Dominik Fuchß <develop@fuchss.org>
2017-07-04 14:33:54 +0200

    Repaired VecParser

--- project/global/src/main/java/rekit/parser/VecParser.java
+++ project/global/src/main/java/rekit/parser/VecParser.java
@@ -15,20 +15,20 @@ import rekit.primitives.geometry.Vec;
  *
  */
 public final class VecParser implements Parser {
-	@Override
-	public Object parseIt(String definition, String[] path) throws Exception {
+    @Override
+    public Object parseIt(String definition, String[] path) throws Exception {
 
-		Pattern pattern = Pattern.compile("[-|\\+]?([0-9]+\\.[0-9]+[f|F]),[-|\\+]?([0-9]+\\.[0-9]+[f|F])");
-		Matcher matcher = pattern.matcher(definition);
-		if (!matcher.find()) {
-			GameConf.GAME_LOGGER.error("BundleHelper: " + definition + " is no Vec");
-			return null;
-		}
-
-		float x = Float.parseFloat(matcher.group(1));
-		float y = Float.parseFloat(matcher.group(2));
-
-		return new Vec(x, y);
-
+	Pattern pattern = Pattern.compile("([-|\\+]?[0-9]+\\.[0-9]+[f|F]),([-|\\+]?[0-9]+\\.[0-9]+[f|F])");
+	Matcher matcher = pattern.matcher(definition);
+	if (!matcher.find()) {
+	    GameConf.GAME_LOGGER.error("BundleHelper: " + definition + " is no Vec");
+	    return null;
 	}
+
+	float x = Float.parseFloat(matcher.group(1));
+	float y = Float.parseFloat(matcher.group(2));
+
+	return new Vec(x, y);
+
+    }
 }
